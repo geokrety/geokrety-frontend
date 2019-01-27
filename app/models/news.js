688062c5d427b94b2cc12297f20f36f6ac0cb85e
Mathieu <github@kumy.net>
2019-01-27 22:33:03 +0100

    Mark news content as safe
    It comes from an admin in legacy app :)

--- app/models/news.js
+++ app/models/news.js
@@ -1,4 +1,10 @@
 import DS from 'ember-data';
+import {
+  htmlSafe
+} from '@ember/template';
+import {
+  computed
+} from '@ember/object';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
@@ -6,5 +12,9 @@ export default DS.Model.extend({
   username: DS.attr('string'),
   commentsCount: DS.attr('number'),
   createdOnDatetime: DS.attr('date'),
-  lastCommentDatetime: DS.attr('date')
+  lastCommentDatetime: DS.attr('date'),
+
+  contentSafe: computed('content', function() {
+    return htmlSafe(`${this.content}`);
+  })
 });
