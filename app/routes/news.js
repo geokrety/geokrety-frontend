c6bfeb9c17cf1461e96b2113e2f975d60f89835f
Mathieu <github@kumy.net>
2019-01-28 23:24:54 +0100

    Infinite scroll for news

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,13 +1,16 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  infinity: service(),
+
   model() {
-    return this.store.query('news', {
-      include: 'author',
+    return this.infinity.model('news', {
+      perPage: 5,
+      startingPage: 1,
+      perPageParam: 'page[size]',
+      pageParam: 'page[number]',
       sort: '-created_on_datetime',
-      page: {
-        size: 10
-      }
     });
   }
 });
